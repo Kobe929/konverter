07c777ffe1941b3386729e02a9050694891d1d58
Filip Dautovic <filipdautovic@yahoo.com>
2016-04-06 10:16:29 +0200

    Kostur

---
+++ Methods.java
@@ -0,0 +1,28 @@
+package konverter;
+
+public class Methods {
+
+	public static void kilogramiFunte(double a) {
+
+	}
+
+	public static void funteKilogrami(double a) {
+
+	}
+
+	public static void centimetarInch(double a) {
+
+	}
+
+	public static void inchCentimetar(double a) {
+
+	}
+
+	public static void celsiusFarenheit(double a) {
+
+	}
+
+	public static void farenheitCelsius(double a) {
+
+	}
+}
